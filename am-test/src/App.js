37c00c957a3288e137dfcbd1016980b644386166
abrilquinterog <abril_quintero@hotmail.com>
2022-07-19 09:37:26 -0500

    Adds style to the characters view

--- am-test/src/App.js
+++ am-test/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './App.scss';
 import Characters from './Characters';
+import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Header/>
       <Characters/>
     </div>
   );
